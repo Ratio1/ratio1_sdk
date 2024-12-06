04597e7e123a3ed9c066ffdaa44d1ac7e70f777a
Andrei Ionut Damian <andrei.damian@me.com>
2024-12-06 09:05:24 +0200

    fix: (HOT) added `NET_CONFIG`

--- naeural_client/const/__init__.py
+++ naeural_client/const/__init__.py
@@ -3,7 +3,7 @@ from . import comms as COMMS
 from . import base as BASE_CT
 from . import payload as PAYLOAD_CT
 from .formatter import FORMATTER_DATA
-from .payload import STATUS_TYPE, PAYLOAD_DATA, COMMANDS, NOTIFICATION_CODES
+from .payload import STATUS_TYPE, PAYLOAD_DATA, COMMANDS, NOTIFICATION_CODES, NET_CONFIG
 from .base import CONFIG_STREAM, BIZ_PLUGIN_DATA, PLUGIN_INFO, BLOCKCHAIN_CONFIG
 from . import heartbeat as HB
 from .environment import ENVIRONMENT
