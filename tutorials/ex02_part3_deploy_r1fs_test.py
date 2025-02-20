4a07a76c9eba582e8f3cd65f81aa6f81e6f18224
Andrei Ionut Damian <andrei.damian@me.com>
2025-02-20 20:33:21 +0200

    feat: work on r1fs tutorial

---
+++ tutorials/ex02_part3_deploy_r1fs_test.py
@@ -0,0 +1,36 @@
+
+from naeural_client import Instance, Payload, Pipeline, Session
+
+
+if __name__ == '__main__':
+
+  session: Session = Session()
+  
+  # this code assumes the node have "allowed" the SDK to deploy the pipeline
+  nodes = [
+    '0xai_A2LfyeItL5oEp7nHONlczGgwS3SV8Ims9ujJ0soJ6Anx',
+    '0xai_AqgKnJMNvUvq5n1wIin_GD2i1FbZ4FBTUJaCI6cWf7i4',
+  ]
+
+  for node in nodes:
+    session.P(f"Deploying pipeline to node: {node}")
+    session.wait_for_node(node) # we wait for the node to be ready
+    pipeline: Pipeline = session.create_pipeline(
+      node=node,
+      name='r1fs_demo_pipeline',
+      data_source='Void',
+    )
+
+    instance: Instance = pipeline.create_plugin_instance(
+      signature='R1FS_DEMO',
+      instance_id='inst01',
+    )
+
+    pipeline.deploy()
+
+  session.wait(
+    seconds=300,            # we wait the session for 60 seconds
+    close_pipelines=True,   # we close the pipelines after the session
+    close_session=True,     # we close the session after the session
+  )
+  session.P("Main thread exiting...")
